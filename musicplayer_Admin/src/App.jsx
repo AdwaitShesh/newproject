062d91bc5228e3a4f8db35af96a0fa67e8e40d57
AdwaitShesh <157292227+AdwaitShesh@users.noreply.github.com>
2024-11-10 15:29:33 +0530

    Update App.jsx

--- musicplayer_Admin/src/App.jsx
+++ musicplayer_Admin/src/App.jsx
@@ -9,7 +9,7 @@ import ListAlbum from './pages/ListAlbum';
 import Sidebar from './components/Sidebar';
 import Navbar from './components/Navbar';
 
-export const url = 'http://localhost:4000'
+export const url = "https://musicplayer-backend-xdob.onrender.com"
 //add
 //song
 const App = () => {
